e24b21e564491308f134d7cd9905bf707224bd80
Shintaro Seki <s2pch.luck@gmail.com>
2016-04-22 13:54:44 +0900

    completion系を更新できるようにした

--- setup-git.sh
+++ setup-git.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-curl -LO https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+COMPLETION_FILES=( git-completion.bash git-prompt.sh )
+
+for file in ${COMPLETION_FILES[@]}; do
+  rm $HOME/dotfiles/$file
+  curl -L -o $HOME/dotfiles/$file https://raw.githubusercontent.com/git/git/master/contrib/completion/$file
+done
 
 if ! [ -f ~/.gitconfig ]; then
     ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 fi
-
