74fad6657fdbcbd8063226902a378249376c3955
Shintaro Seki <s2pch.luck@gmail.com>
2026-06-17 15:24:36 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-set -eu
+set -eu -o pipefail -o posix
 
-: ${DEFAULT_TARGETS:=vim tmux git bash}
+: "${DEFAULT_TARGETS:=vim git zsh}"
 
 run() { echo "RUN: $*"; "$@"; }
-log.i() { echo "INFO: $*"; }
-log.e() { echo "ERROR: $*"; }
+log_i() { echo "INFO: $*"; }
+log_e() { echo "ERROR: $*"; }
 
 apply_to_files() {
   local cmd="$1"; shift
   while (($# > 0)); do
     local file="$1"; shift
     if [[ -a "${HOME}/${file}" ]]; then
-      log.i "Skip ~/${file} that already exists."
+      log_i "Skip ~/${file} that already exists."
       continue
     fi
     run $cmd "${HOME}/dotfiles/${file}" "${HOME}/${file}"
@@ -22,7 +22,7 @@ apply_to_files() {
 link() { apply_to_files 'ln -s' "$@"; }
 copy() { apply_to_files 'cp' "$@"; }
 
-setup.bash() {
+setup_bash() {
   # source bash files
   local file
   for file in \
@@ -35,7 +35,7 @@ setup.bash() {
   done
 }
 
-setup.zsh() {
+setup_zsh() {
   local file
   for file in \
     .zprofile .zshrc .zshenv
@@ -47,30 +47,30 @@ setup.zsh() {
   done
 }
 
-setup.fish() {
+setup_fish() {
   # NOTE: Referring XDG_CONFIG_HOME would be better.
   mkdir -p "${HOME}/.config/fish"
   link .config/fish/*
 }
 
-setup.tmux() {
+setup_tmux() {
   git submodule update --init .tmux/plugins/tpm
   link .tmux.conf .tmux
 }
 
-setup.git() {
+setup_git() {
   copy .gitconfig
 }
 
-setup.vim() {
+setup_vim() {
   link .vimrc .vim dein.toml
 }
 
-setup.emacs() {
+setup_emacs() {
   link .emacs.d
 }
 
-setup.atom() {
+setup_atom() {
   mkdir -p "${HOME}/.atom"
   for file in \
     config.cson init.coffee keymap.cson snippets.cson styles.less
@@ -81,9 +81,9 @@ setup.atom() {
   done
 }
 
-setup.vscode() {
+setup_vscode() {
   if [[ "$(uname)" != Darwin ]]; then
-    log.i 'Not supported now.'
+    log_i 'Not supported now.'
     return
   fi
   local vscode_user_dir="${HOME}/Library/Application Support/Code/User"
@@ -100,9 +100,9 @@ setup.vscode() {
   defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
 }
 
-setup.karabiner() {
+setup_karabiner() {
   if [[ "$(uname)" != Darwin ]]; then
-    log.i 'Only macOS supported, skipped.'
+    log_i 'Only macOS supported, skipped.'
     return
   fi
   local p='.config/karabiner/assets/complex_modifications'
@@ -110,12 +110,25 @@ setup.karabiner() {
   ln -s "${HOME}/dotfiles/${p}/my_default.json" "${HOME}/${p}/"
 }
 
-setup.starship() {
+setup_starship() {
   mkdir -p "${HOME}/.config"
   ln -s "${HOME}/dotfiles/starship.toml" "${HOME}/.config/starship.toml"
 }
 
-while IFS=$'\n' read -r line; do TARGETS+=("$line"); done < <(compgen -A function setup. | cut -d. -f2)
+setup_pwsh() {
+  local file
+  # shellcheck disable=SC2016
+  file="$(pwsh -Command '$PROFILE')"
+  # shellcheck disable=SC2016
+  local line='. "$HOME/dotfiles/pwsh/Microsoft.PowerShell_profile.ps1"'
+
+  mkdir -p "$(dirname "$file")"
+  if ! (grep "$line" "$file" >/dev/null); then
+    printf '%s\n\n' "$line" >>"$file"
+  fi
+}
+
+while IFS=$'\n' read -r line; do TARGETS+=("$line"); done < <(compgen -A function setup_ | cut -d. -f2)
 
 help() {
   cat <<EOT
@@ -138,15 +151,15 @@ main() {
       -h | --help ) help; exit 1;;
       -l | --list ) list; exit;;
       -a | --all | all ) targets=("${TARGETS[@]}");;
-      -* ) log.e "Unknown option: $1"; exit 1;;
+      -* ) log_e "Unknown option: $1"; exit 1;;
       * ) targets=("$@");;
     esac
   fi
-  log.i "Targets: ${targets[*]}"
+  log_i "Targets: ${targets[*]}"
   local target
   for target in "${targets[@]}"; do
-    log.i "Setup ${target}..."
-    "setup.${target}"
+    log_i "Setup ${target}..."
+    "setup_${target}"
   done
 }
 
